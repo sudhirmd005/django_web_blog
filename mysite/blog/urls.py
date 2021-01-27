a58d97f6bf5b168e696a35f0664a7f8df1140289
Sudhir <sudhir05.sd@gmail.com>
2021-01-27 14:17:29 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_line, name="post_line"),
+    path('posts/<int:pk>/', views.post_details, name="post_details"),
+]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+#from django.http import HttpResponse, Http404
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_line(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_line.html', {'posts': posts})
+def post_details(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_details.html', {'posts': posts})
