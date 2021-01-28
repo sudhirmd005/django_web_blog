895ac0c1df29ae18627c1254056f99b9f453e2e0
sudhirmd005 <sudhird621@gmail.com>
2021-01-28 14:28:11 +0530

    final

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_line, name="post_line"),
     path('posts/<int:pk>/', views.post_details, name="post_details"),
-    path('post/new/', views.post_new, name='post_new'),
+    path('posts/new/', views.post_new, name='post_new'),
+    path('posts/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,11 +20,24 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_details', pk=post.pk)
+            posts = form.save(commit=False)
+            posts.author = request.user
+            posts.published_date = timezone.now()
+            posts.save()
+            return redirect('post_details', pk=posts.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+def post_edit(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=posts)
+        if form.is_valid():
+            posts = form.save(commit=False)
+            posts.author = request.user
+            posts.published_date = timezone.now()
+            posts.save()
+            return redirect('post_details', pk=posts.pk)
+    else:
+        form = PostForm(instance=posts)
     return render(request, 'blog/post_edit.html', {'form': form})
